0f9526b44e895308b44de5ca93fc72d9af78e00a
enriqueipn27 <94320296+enriqueipn27@users.noreply.github.com>
2026-04-12 01:21:16 -0600

    Update tinaco_alexa_bridge.py

--- tinaco_alexa_bridge.py
+++ tinaco_alexa_bridge.py
@@ -2,16 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# 🔐 TOKEN
-@app.route("/token", methods=["POST"])
-def token():
-    return jsonify({
-        "access_token": "dummy-token",
-        "token_type": "Bearer",
-        "expires_in": 3600
-    })
-
-# 🔐 AUTH
 @app.route("/auth")
 def auth():
     redirect_uri = request.args.get("redirect_uri")
@@ -21,16 +11,20 @@ def auth():
 
     return f'''
     <html>
-    <body>
-    <h2>Vinculando cuenta...</h2>
     <script>
     window.location.href = "{redirect_uri}?state={state}&code=1234";
     </script>
-    </body>
     </html>
     '''
 
-# (opcional pero recomendado)
+@app.route("/token", methods=["POST"])
+def token():
+    return jsonify({
+        "access_token": "dummy-token",
+        "token_type": "Bearer",
+        "expires_in": 3600
+    })
+
 @app.route("/debug")
 def debug():
     return jsonify({
